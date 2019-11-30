96d7b3ddc5441e54a840d39d01a186759dc58b25
htdingkang <ht_dingkang@126.com>
2019-11-30 23:36:58 +0800

    商品信息录入，平台属性，属性值新增，修改

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/AttrServiceImpl.java
@@ -0,0 +1,64 @@
+package com.atguigu.gmall.manage.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.PmsBaseAttrInfo;
+import com.atguigu.gmall.bean.PmsBaseAttrValue;
+import com.atguigu.gmall.manage.mapper.PmsBaseAttrInfoMapper;
+import com.atguigu.gmall.manage.mapper.PmsBaseAttrValueMapper;
+import com.atguigu.gmall.service.AttrService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+@Service
+public class AttrServiceImpl implements AttrService {
+
+    @Autowired
+    PmsBaseAttrInfoMapper pmsBaseAttrInfoMapper;
+    @Autowired
+    PmsBaseAttrValueMapper pmsBaseAttrValueMapper;
+
+    @Override
+    public List<PmsBaseAttrInfo> attrInfoList(String catalog3Id) {
+        PmsBaseAttrInfo pmsBaseAttrInfo = new PmsBaseAttrInfo();
+        pmsBaseAttrInfo.setCatalog3Id(catalog3Id);
+        List<PmsBaseAttrInfo> select = pmsBaseAttrInfoMapper.select(pmsBaseAttrInfo);
+        return select;
+    }
+
+    @Override
+    public String saveAttrInfo(PmsBaseAttrInfo pmsBaseAttrInfo) {
+        String id =pmsBaseAttrInfo.getId();
+        if (StringUtils.isBlank(id)) {
+            pmsBaseAttrInfoMapper.insertSelective(pmsBaseAttrInfo);
+            for (PmsBaseAttrValue pmsBaseAttrValue:pmsBaseAttrInfo.getAttrValueList()) {
+                pmsBaseAttrValue.setAttrId(pmsBaseAttrInfo.getId());
+                pmsBaseAttrValueMapper.insertSelective(pmsBaseAttrValue);
+            }
+        } else {
+            Example example=new Example(PmsBaseAttrInfo.class);
+            example.createCriteria().andEqualTo("id",pmsBaseAttrInfo.getId());
+            pmsBaseAttrInfoMapper.updateByExampleSelective(pmsBaseAttrInfo,example);
+
+            //注意这里包含了前台页面删除PmsBaseAttrValue的操作
+            PmsBaseAttrValue pmsBaseAttrValueDel=new PmsBaseAttrValue();
+            pmsBaseAttrValueDel.setAttrId(pmsBaseAttrInfo.getId());
+            pmsBaseAttrValueMapper.delete(pmsBaseAttrValueDel);
+
+            List<PmsBaseAttrValue> attrValueList = pmsBaseAttrInfo.getAttrValueList();
+            for (PmsBaseAttrValue pmsBaseAttrValue : attrValueList) {
+                pmsBaseAttrValue.setAttrId(pmsBaseAttrInfo.getId());
+                pmsBaseAttrValueMapper.insertSelective(pmsBaseAttrValue);
+            }
+        }
+        return "success";
+    }
+
+    @Override
+    public List<PmsBaseAttrValue> getAttrValueList(String attrId) {
+        PmsBaseAttrValue pmsBaseAttrValue=new PmsBaseAttrValue();
+        pmsBaseAttrValue.setAttrId(attrId);
+        return pmsBaseAttrValueMapper.select(pmsBaseAttrValue);
+    }
+}
