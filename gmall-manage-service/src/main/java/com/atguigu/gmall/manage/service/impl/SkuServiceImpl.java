a4a57eae7af9d103306c9cb2e3fbb3844c2037d2
htdingkang <ht_dingkang@126.com>
2019-12-06 11:09:16 +0800

    redis nx 实现的分布式锁的优化，处理解锁过程中的一些问题。

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuServiceImpl.java
@@ -17,7 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import redis.clients.jedis.Jedis;
 import tk.mybatis.mapper.util.StringUtil;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.UUID;
+
 @Service
 public class SkuServiceImpl implements SkuService {
     @Autowired
@@ -84,7 +87,9 @@ public class SkuServiceImpl implements SkuService {
             //缓存中没有，查询数据库
             //为防止缓存击穿，设置分布式锁
             String lockKey="sku:"+skuId+":lock";
-            String OK = jedis.set(lockKey, "1", "NX", "PX", 10000);
+            //添加锁的标识
+            String token = UUID.randomUUID().toString();
+            String OK = jedis.set(lockKey, token, "NX", "PX", 10000);
             if(StringUtils.isNotEmpty(OK)&& "OK".equals(OK)){
                 //设置成功有权访问数据库
                 pmsSkuInfo = getSkuByIdFromDb(skuId);
@@ -96,12 +101,22 @@ public class SkuServiceImpl implements SkuService {
                     //设置一个较短过期时间，短期内不可再访问数据库
                     jedis.setex(skukey,3*60,JSON.toJSONString(""));
                 }
-                jedis.del(lockKey);  //解锁
+                //为了防止线程阻塞，还未返回，lock已经过期释放，并且被别人获取到，此时不能删除别人的锁
+                //String lockToken = jedis.get(lockKey);
+                //if(StringUtils.isNotEmpty(lockToken)&&token.equals(lockToken)){
+                //    jedis.del(lockKey);  //解锁,删除自己的锁
+                //}
+
+                //甚至取回lockToken后进行判断的瞬间，lockKey失效，然后被别人拿到锁，set lockKey成功，此时还是不能删除别人的锁
+                //于是可以使用lua脚本，去查lockKey的value时，如果是自己的token，立马在redis端就删除掉
+                //redis lua脚本格式 eval script numkeys key [key ...] arg [arg ...]
+                String script = "if redis.call('get',KEYS[1])==ARGV[1] then return redis.call('del',KEYS[1]) else return 0 end";
+                jedis.eval(script, Collections.singletonList(lockKey),Collections.singletonList(token));
 
             }else{
                 //设置失败，自旋（该线程在睡眠几秒后重新尝试访问本方法）
                 try {
-                    Thread.sleep(3000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
